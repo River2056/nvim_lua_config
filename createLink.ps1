d9aba41c4ae3f04407eec32c3c493efb07582126
Kevin.Tung <chen0625tung@gmail.com>
2022-08-22 02:27:19 +0800

    + added powershell script for consistent link creation

---
+++ createLink.ps1
@@ -0,0 +1,42 @@
+New-Item -ItemType directory -Path C:\Users\User\AppData\Local\nvim\lua\kevin
+New-Item -ItemType directory -Path C:\Users\User\AppData\Local\nvim\after
+New-Item -ItemType directory -Path C:\Users\User\AppData\Local\nvim\after\plugin
+New-Item -ItemType directory -Path C:\Users\User\AppData\Local\nvim\ftplugin
+New-Item -ItemType directory -Path C:\Users\User\AppData\Local\nvim\plugin
+
+New-Item -ItemType SymbolicLink -Path C:\Users\User\AppData\Local\nvim\after\plugin\harpoon.rc.lua -Target C:\Users\User\nvim_lua_config\after\plugin\harpoon.rc.lua
+New-Item -ItemType SymbolicLink -Path C:\Users\User\AppData\Local\nvim\after\plugin\lualine.rc.lua -Target C:\Users\User\nvim_lua_config\after\plugin\lualine.rc.lua
+New-Item -ItemType SymbolicLink -Path C:\Users\User\AppData\Local\nvim\after\plugin\neogit.rc.lua -Target C:\Users\User\nvim_lua_config\after\plugin\neogit.rc.lua
+New-Item -ItemType SymbolicLink -Path C:\Users\User\AppData\Local\nvim\after\plugin\nvim-cmp.rc.lua -Target C:\Users\User\nvim_lua_config\after\plugin\nvim-cmp.rc.lua
+New-Item -ItemType SymbolicLink -Path C:\Users\User\AppData\Local\nvim\after\plugin\nvim-dap-ui.rc.lua -Target C:\Users\User\nvim_lua_config\after\plugin\nvim-dap-ui.rc.lua
+New-Item -ItemType SymbolicLink -Path C:\Users\User\AppData\Local\nvim\after\plugin\nvim-dap.rc.lua -Target C:\Users\User\nvim_lua_config\after\plugin\nvim-dap.rc.lua
+New-Item -ItemType SymbolicLink -Path C:\Users\User\AppData\Local\nvim\after\plugin\nvim-surround.rc.lua -Target C:\Users\User\nvim_lua_config\after\plugin\nvim-surround.rc.lua
+New-Item -ItemType SymbolicLink -Path C:\Users\User\AppData\Local\nvim\after\plugin\nvim-tree.rc.lua -Target C:\Users\User\nvim_lua_config\after\plugin\nvim-tree.rc.lua
+New-Item -ItemType SymbolicLink -Path C:\Users\User\AppData\Local\nvim\after\plugin\nvim-treesitter.rc.lua -Target C:\Users\User\nvim_lua_config\after\plugin\nvim-treesitter.rc.lua
+New-Item -ItemType SymbolicLink -Path C:\Users\User\AppData\Local\nvim\after\plugin\telescope-ui-select.rc.lua -Target C:\Users\User\nvim_lua_config\after\plugin\telescope-ui-select.rc.lua
+New-Item -ItemType SymbolicLink -Path C:\Users\User\AppData\Local\nvim\after\plugin\telescope.rc.lua -Target C:\Users\User\nvim_lua_config\after\plugin\telescope.rc.lua
+New-Item -ItemType SymbolicLink -Path C:\Users\User\AppData\Local\nvim\after\plugin\rest.rc.lua -Target C:\Users\User\nvim_lua_config\after\plugin\rest.rc.lua
+
+New-Item -ItemType SymbolicLink -Path C:\Users\User\AppData\Local\nvim\ftplugin\java.lua -Target C:\Users\User\nvim_lua_config\ftplugin\java.lua
+New-Item -ItemType SymbolicLink -Path C:\Users\User\AppData\Local\nvim\ftplugin\python.lua -Target C:\Users\User\nvim_lua_config\ftplugin\python.lua
+New-Item -ItemType SymbolicLink -Path C:\Users\User\AppData\Local\nvim\ftplugin\javascript.lua -Target C:\Users\User\nvim_lua_config\ftplugin\javascript.lua
+New-Item -ItemType SymbolicLink -Path C:\Users\User\AppData\Local\nvim\ftplugin\typescript.lua -Target C:\Users\User\nvim_lua_config\ftplugin\typescript.lua
+New-Item -ItemType SymbolicLink -Path C:\Users\User\AppData\Local\nvim\ftplugin\vue.lua -Target C:\Users\User\nvim_lua_config\ftplugin\vue.lua
+New-Item -ItemType SymbolicLink -Path C:\Users\User\AppData\Local\nvim\ftplugin\go.lua -Target C:\Users\User\nvim_lua_config\ftplugin\go.lua
+
+New-Item -ItemType SymbolicLink -Path C:\Users\User\AppData\Local\nvim\lua\kevin\init.lua -Target C:\Users\User\nvim_lua_config\lua\kevin\init.lua
+New-Item -ItemType SymbolicLink -Path C:\Users\User\AppData\Local\nvim\lua\kevin\constants.lua -Target C:\Users\User\nvim_lua_config\lua\kevin\constants.lua
+New-Item -ItemType SymbolicLink -Path C:\Users\User\AppData\Local\nvim\lua\abbreviations.lua -Target C:\Users\User\nvim_lua_config\lua\abbreviations.lua
+New-Item -ItemType SymbolicLink -Path C:\Users\User\AppData\Local\nvim\lua\augroups.lua -Target C:\Users\User\nvim_lua_config\lua\augroups.lua
+New-Item -ItemType SymbolicLink -Path C:\Users\User\AppData\Local\nvim\lua\base.lua -Target C:\Users\User\nvim_lua_config\lua\base.lua
+New-Item -ItemType SymbolicLink -Path C:\Users\User\AppData\Local\nvim\lua\functions.lua -Target C:\Users\User\nvim_lua_config\lua\functions.lua
+New-Item -ItemType SymbolicLink -Path C:\Users\User\AppData\Local\nvim\lua\macos.lua -Target C:\Users\User\nvim_lua_config\lua\macos.lua
+New-Item -ItemType SymbolicLink -Path C:\Users\User\AppData\Local\nvim\lua\maps.lua -Target C:\Users\User\nvim_lua_config\lua\maps.lua
+New-Item -ItemType SymbolicLink -Path C:\Users\User\AppData\Local\nvim\lua\plugins.lua -Target C:\Users\User\nvim_lua_config\lua\plugins.lua
+New-Item -ItemType SymbolicLink -Path C:\Users\User\AppData\Local\nvim\lua\utils.lua -Target C:\Users\User\nvim_lua_config\lua\utils.lua
+New-Item -ItemType SymbolicLink -Path C:\Users\User\AppData\Local\nvim\lua\windows.lua -Target C:\Users\User\nvim_lua_config\lua\windows.lua
+
+New-Item -ItemType SymbolicLink -Path C:\Users\User\AppData\Local\nvim\plugin\lspconfig.lua -Target C:\Users\User\nvim_lua_config\plugin\lspconfig.lua
+New-Item -ItemType SymbolicLink -Path C:\Users\User\AppData\Local\nvim\plugin\null-ls.rc.lua -Target C:\Users\User\nvim_lua_config\plugin\null-ls.rc.lua
+
+New-Item -ItemType SymbolicLink -Path C:\Users\User\AppData\Local\nvim\init.lua -Target C:\Users\User\nvim_lua_config\init.lua
