a9c676acc4cf4e7ab0662b5d5b28914490e15f25
Kevin_Tung__ <kevin.tung@hytechtech.com>
2022-09-05 11:10:04 +0800

    + added more harpoon config for better file navigation

--- after/plugin/harpoon.rc.lua
+++ after/plugin/harpoon.rc.lua
@@ -5,3 +5,7 @@ vim.keymap.set("n", "<Leader>hh", ':lua require("harpoon.ui").toggle_quick_menu(
 vim.keymap.set("n", "<Leader>ha", ':lua require("harpoon.mark").add_file()<Return>')
 vim.keymap.set("n", "<Leader>hn", ':lua require("harpoon.ui").nav_next()<Return>')
 vim.keymap.set("n", "<Leader>hp", ':lua require("harpoon.ui").nav_prev()<Return>')
+vim.keymap.set("n", "<Leader>hq", ':lua require("harpoon.ui").nav_file(1)<Return>')
+vim.keymap.set("n", "<Leader>hw", ':lua require("harpoon.ui").nav_file(2)<Return>')
+vim.keymap.set("n", "<Leader>he", ':lua require("harpoon.ui").nav_file(3)<Return>')
+vim.keymap.set("n", "<Leader>hr", ':lua require("harpoon.ui").nav_file(4)<Return>')
