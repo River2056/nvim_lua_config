611581006da8384144acc01c8f6be377913e5509
Kevin_Tung__ <kevin.tung@hytechtech.com>
2022-10-17 16:13:26 +0800

    + change from update_capabilities => default_capabilities

--- ftplugin/kotlin.lua
+++ ftplugin/kotlin.lua
@@ -1,7 +1,7 @@
 local util = require("lspconfig/util")
 
 local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities = require("cmp_nvim_lsp").update_capabilities(capabilities)
+capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
 
 local on_attach = function(client, bufnr)
     -- Create some shortcut functions.
