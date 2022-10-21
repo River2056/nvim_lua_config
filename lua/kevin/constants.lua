50988e95adbc96ce1b89056d266e4419a123c137
Kevin_Tung__ <kevin.tung@hytechtech.com>
2022-10-21 11:07:41 +0800

    + modified constants home path to use enviroment variable KEVIN_NVIM_HOME instead of hardcoded value

--- lua/kevin/constants.lua
+++ lua/kevin/constants.lua
@@ -1,6 +1,9 @@
 local M = {}
 
-M.home = vim.fn.has("macunix") == 1 and "~" or "C:/Users/H0268"
+-- M.home = vim.fn.has("macunix") == 1 and "~" or "C:/Users/H0268"
+-- get home path from environment variable, set before use!
+M.home = os.getenv("KEVIN_NVIM_HOME")
+
 M.config_path = vim.fn.stdpath("config"):gsub("\\", "/")
 M.java_debug_path = M.config_path .. "/java-debug/"
 M.vscode_java_test_path = M.config_path .. "/vscode-java-test/"
--- lua/kevin/init.lua
+++ lua/kevin/init.lua
@@ -1,37 +1,36 @@
-local install_path = vim.fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
-local config_path = vim.fn.stdpath('config')
-local java_debug_path = config_path .. '/java-debug/'
-local vscode_java_test_path = config_path .. '/vscode-java-test/'
-local google_java_format_path = config_path .. '/google-java-format/'
+local install_path = vim.fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
+local config_path = vim.fn.stdpath("config")
+local java_debug_path = config_path .. "/java-debug/"
+local vscode_java_test_path = config_path .. "/vscode-java-test/"
+local google_java_format_path = config_path .. "/google-java-format/"
 
 if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
-    print('cloning packer...')
-    vim.fn.execute(
-        '!git clone https://github.com/wbthomason/packer.nvim ' .. install_path
-    )
+	print("cloning packer...")
+	vim.fn.execute("!git clone https://github.com/wbthomason/packer.nvim " .. install_path)
 end
 
 if vim.fn.empty(vim.fn.glob(java_debug_path)) > 0 then
-    print('cloning java-debug...')
-    print('please cd into java-debug and mvnw clean install (mvnw.cmd clean install for windows)')
-    vim.fn.execute(
-        '!git clone https://github.com/microsoft/java-debug.git ' .. java_debug_path
-    )
+	print("cloning java-debug...")
+	print("please cd into java-debug and mvnw clean install (mvnw.cmd clean install for windows)")
+	vim.fn.execute("!git clone https://github.com/microsoft/java-debug.git " .. java_debug_path)
 end
 
 if vim.fn.empty(vim.fn.glob(vscode_java_test_path)) > 0 then
-    print('cloning vscode-java-test...')
-    print('please cd into vscode-java-test and npm install')
-    vim.fn.execute(
-        '!git clone https://github.com/microsoft/vscode-java-test.git ' .. vscode_java_test_path
-    )
+	print("cloning vscode-java-test...")
+	print("please cd into vscode-java-test and npm install")
+	vim.fn.execute("!git clone https://github.com/microsoft/vscode-java-test.git " .. vscode_java_test_path)
 end
 
 if vim.fn.empty(vim.fn.glob(google_java_format_path)) > 0 then
-    print('cloning google-java-format...')
-    print('please mvn install after clone')
-    print('or npm install google-java-format -g to path')
-    vim.fn.execute(
-        '!git clone https://github.com/google/google-java-format.git ' .. google_java_format_path
-    )
+	print("cloning google-java-format...")
+	print("please mvn install after clone")
+	print("or npm install google-java-format -g to path")
+	vim.fn.execute("!git clone https://github.com/google/google-java-format.git " .. google_java_format_path)
+end
+
+local home_var = os.getenv("KEVIN_NVIM_HOME")
+if home_var == nil or home_var == "" then
+	print("environment variable KEVIN_NVIM_HOME not set!")
+	print("please set KEVIN_NVIM_HOME for nvim config to work!")
+	print("e.g. on windows: C:/Users/H0268, on linux: /home/kali")
 end
