5950446e1dededfe2e9cdecd5e5be729cf5d0f10
river2056 <chen0625tung@gmail.com>
2022-12-03 13:38:34 +0800

    + added macbook_air specific settings

--- lua/kevin/constants.lua
+++ lua/kevin/constants.lua
@@ -9,11 +9,11 @@ M.java_debug_path = M.config_path .. "/java-debug/"
 M.vscode_java_test_path = M.config_path .. "/vscode-java-test/"
 M.lombok_path = M.home .. "/lombok.jar"
 
-M.jdtls_java_path = "C:/Program Files/OpenJDK/jdk-17.0.2/bin/java.exe"
-M.jdtls_jdk11_path = "C:/Program Files/Eclipse Adoptium/jdk-11.0.16.8-hotspot"
-M.jdtls_jdk15_path = "C:/openjdk-15.0.2_windows-x64_bin/jdk-15.0.2"
-M.jdtls_jdk17_path = "C:/Program Files/OpenJDK/jdk-17.0.2"
-M.jdtls_jdk8_path = "C:/Program Files/Zulu/zulu-8"
+M.jdtls_java_path = "/opt/homebrew/opt/openjdk@17/bin/java"
+M.jdtls_jdk11_path = "/opt/homebrew/opt/openjdk@11"
+-- M.jdtls_jdk15_path = "C:/openjdk-15.0.2_windows-x64_bin/jdk-15.0.2"
+M.jdtls_jdk17_path = "/opt/homebrew/opt/openjdk@17"
+-- M.jdtls_jdk8_path = "C:/Program Files/Zulu/zulu-8"
 M.jdtls_debug_port = 8000
 M.powershell_es_path = vim.fn.stdpath("data"):gsub("\\", "/") .. "/mason/packages/powershell-editor-services/"
 
