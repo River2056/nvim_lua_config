ff4b8b0cd34d2e1f3cc9ce7a08aa2bf8fee56b82
kevin.tung <chen0625tung@gmail.com>
2022-12-04 12:27:25 +0800

    + added branch rog

--- lua/kevin/constants.lua
+++ lua/kevin/constants.lua
@@ -10,10 +10,10 @@ M.vscode_java_test_path = M.config_path .. "/vscode-java-test/"
 M.lombok_path = M.home .. "/lombok.jar"
 
 M.jdtls_java_path = "C:/Program Files/OpenJDK/jdk-17.0.2/bin/java.exe"
-M.jdtls_jdk11_path = "C:/Program Files/Eclipse Adoptium/jdk-11.0.16.8-hotspot"
-M.jdtls_jdk15_path = "C:/openjdk-15.0.2_windows-x64_bin/jdk-15.0.2"
+M.jdtls_jdk11_path = "C:/Program Files/Eclipse Adoptium/jdk-11.0.13.8-hotspot/"
+M.jdtls_jdk15_path = "C:/Program Files/Java/jdk-15.0.1"
 M.jdtls_jdk17_path = "C:/Program Files/OpenJDK/jdk-17.0.2"
-M.jdtls_jdk8_path = "C:/Program Files/Zulu/zulu-8"
+M.jdtls_jdk8_path = "C:/Program Files/Java/jdk1.8.0_281"
 M.jdtls_debug_port = 8000
 M.powershell_es_path = vim.fn.stdpath("data"):gsub("\\", "/") .. "/mason/packages/powershell-editor-services/"
 
