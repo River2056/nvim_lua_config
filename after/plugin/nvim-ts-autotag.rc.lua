63904b5f5573cb6ca2cbb0761863574c5c63fbeb
Kevin_Tung__ <kevin.tung@hytechtech.com>
2022-12-07 15:20:22 +0800

    + added filetypes for nvim-ts-autotag

--- after/plugin/nvim-ts-autotag.rc.lua
+++ after/plugin/nvim-ts-autotag.rc.lua
@@ -1,1 +1,20 @@
-require('nvim-ts-autotag').setup()
+require("nvim-ts-autotag").setup({
+	filetypes = {
+		"html",
+		"javascript",
+		"typescript",
+		"javascriptreact",
+		"typescriptreact",
+		"svelte",
+		"vue",
+		"tsx",
+		"jsx",
+		"rescript",
+		"xml",
+		"php",
+		"markdown",
+		"glimmer",
+		"handlebars",
+		"hbs",
+	},
+})
