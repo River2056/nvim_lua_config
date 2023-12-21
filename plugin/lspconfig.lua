744fd4776ddaabe7291fb09734ef6df564600e19
river2056 <chen0625tung@gmail.com>
2023-12-22 00:34:46 +0800

    added c sharp support

--- plugin/lspconfig.lua
+++ plugin/lspconfig.lua
@@ -83,14 +83,14 @@ end
 
 -- specific additional configs per language
 nvim_lsp.lua_ls.setup({
-	on_attach = lsp.on_attach,
-	capabilities = lsp.capabilities,
-	settings = {
-		Lua = {
-			diagnostics = {
-				-- Get the language server to recognize the `vim` global
-				globals = { "vim" },
-			},
+    on_attach = lsp.on_attach,
+    capabilities = lsp.capabilities,
+    settings = {
+        Lua = {
+            diagnostics = {
+                -- Get the language server to recognize the `vim` global
+                globals = { "vim" },
+            },
 
 			workspace = {
 				-- Make the server aware of Neovim runtime files
@@ -120,4 +120,47 @@ nvim_lsp.tsserver.setup({
 nvim_lsp.ccls.setup({
 	on_attach = lsp.on_attach,
 	capabilities = lsp.capabilities,
+})
+
+local pid = vim.fn.getpid()
+local mason_path = vim.fn.stdpath("data") .. "/mason/bin"
+nvim_lsp.omnisharp.setup({
+    handlers = {
+        ["textDocument/definition"] = require("omnisharp_extended").handler,
+    },
+    cmd = { mason_path .. "/omnisharp", "--languageserver", "--hostPID", tostring(pid) },
+    -- Enables support for reading code style, naming convention and analyzer
+    -- settings from .editorconfig.
+    enable_editorconfig_support = true,
+
+    -- If true, MSBuild project system will only load projects for files that
+    -- were opened in the editor. This setting is useful for big C# codebases
+    -- and allows for faster initialization of code navigation features only
+    -- for projects that are relevant to code that is being edited. With this
+    -- setting enabled OmniSharp may load fewer projects and may thus display
+    -- incomplete reference lists for symbols.
+    enable_ms_build_load_projects_on_demand = false,
+
+    -- Enables support for roslyn analyzers, code fixes and rulesets.
+    enable_roslyn_analyzers = false,
+
+    -- Specifies whether 'using' directives should be grouped and sorted during
+    -- document formatting.
+    organize_imports_on_format = false,
+
+    -- Enables support for showing unimported types and unimported extension
+    -- methods in completion lists. When committed, the appropriate using
+    -- directive will be added at the top of the current file. This option can
+    -- have a negative impact on initial completion responsiveness,
+    -- particularly for the first few completion sessions after opening a
+    -- solution.
+    enable_import_completion = false,
+
+    -- Specifies whether to include preview versions of the .NET SDK when
+    -- determining which version to use for project loading.
+    sdk_include_prereleases = true,
+
+    -- Only run analyzers against open files when 'enableRoslynAnalyzers' is
+    -- true
+    analyze_open_documents_only = false,
 })
