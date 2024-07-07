083a4dbda45eefa06565682e6e247d335a50bb43
Kevin_Tung <chen0625tung@gmail.com>
2024-07-08 01:05:50 +0800

    added
    + use eslint_d for typescript format in guard.nvim

--- after/plugin/guard.rc.lua
+++ after/plugin/guard.rc.lua
@@ -2,11 +2,13 @@ local ft = require("guard.filetype")
 
 ft("python"):fmt("black")
 ft("lua"):fmt("lsp"):append("stylua")
-ft('typescript,javascript,typescriptreact'):fmt('prettier')
+-- ft('typescript,javascript,typescriptreact'):fmt('prettier')
+ft('typescript,javascript,typescriptreact'):fmt('eslint_d')
 -- ft('html'):fmt('html-lsp')
 -- ft('html'):fmt("lsp"):append("prettier")
 ft("cs"):fmt("lsp"):append("csharpier")
 ft("c"):fmt("lsp"):append("clang-format")
+ft("go"):fmt("gofumpt")
 
 require("guard").setup({
     -- the only options for the setup function
